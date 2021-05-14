80db5a2d7e9d5ca99f251eeec4df6cae8ccfd21c
Robin Ferm <Frolleferm@hotmail.com>
2021-05-14 10:36:19 +0200

    Added new PlayerController so we can add new players to the game

---
+++ Source/LudoV2Api/Models/ApiRequests/NewPlayerRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LudoV2Api.Models.ApiRequests
+{
+    public class NewPlayerRequest
+    {
+        public string PlayerName { get; set; }
+    }
+}
